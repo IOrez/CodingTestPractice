b66720adc62076134f953a76d06f83fa9c0ede2b
IOrez <h.5363@daum.net>
2020-08-12 15:49:51 +0900

    백준 17162 가희의 수열놀이 분류 변경 스택-> 그리디 알고리즘

--- CodingTestPractice/Baekjoon/그리디 알고리즘/17162.hpp
+++ CodingTestPractice/Baekjoon/그리디 알고리즘/17162.hpp
@@ -9,21 +9,21 @@ int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(nullptr);
 	cin >> Q >> Mod;
-	vector<int> check(Mod,0);
+	vector<int> check(Mod, 0);
 	vector<list<int>> table(Mod);
 	int num = 0, order = 0;
 
-	for (int i = 0; i < Q; ++i){
+	for (int i = 0; i < Q; ++i) {
 		cin >> num;
 		switch (num) {
 		case 1:
 			cin >> num; order++;
-			table[num%Mod].insert(table[num % Mod].begin(), order);
+			table[num % Mod].insert(table[num % Mod].begin(), order);
 			break;
 		case 2:
 			for (int i = 0; i < Mod; ++i) {
 				if (table[i].empty())continue;
-				if (*table[i].begin() == order) { 
+				if (*table[i].begin() == order) {
 					table[i].erase(table[i].begin());
 					order--;
 					break;
@@ -33,7 +33,7 @@ int main()
 		case 3:
 			bool check = true;
 			int later = order;
-			for (int i = 0; i < Mod&&check; ++i) {
+			for (int i = 0; i < Mod && check; ++i) {
 				if (table[i].empty())check = false;
 				else
 					later = min(later, *table[i].begin());
