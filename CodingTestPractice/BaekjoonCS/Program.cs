712bb73005129c205f18980af510e424bc32c4ad
IOrez <h.5363@daum.net>
2021-10-09 18:08:20 +0900

    백준 문자열 10808 => C#을 위한 환경을 구성하였고 쉬운 문제를 통해 개발환경을 테스트하였다. 문제는 단순히 단어를 이루는 알파벳의 갯수를 알파벳별로 출력하는 것이다.

---
+++ CodingTestPractice/BaekjoonCS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonCS
+{
+    class _10808
+    {
+        String input;
+        int[] Cnt;
+
+        private void Init()
+        {
+            Cnt = Enumerable.Repeat((int)0, 'z' - 'a' + 1).ToArray();
+            input = Console.ReadLine();
+        }
+
+        public void Solution()
+        {
+            Init();
+            foreach (char c in input)
+                Cnt[c - 'a']++;
+            foreach (int i in Cnt)
+                Console.Write(i + " ");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _10808 problem = new _10808();
+            problem.Solution();
+        }
+    }
+}
---
+++ CodingTestPractice/BaekjoonCS/문자열/10808.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonCS.문자열
+{
+    class _10808
+    {
+        String input;
+        int[] Cnt;
+
+        private void Init()
+        {
+            Cnt = Enumerable.Repeat((int)0, 'z' - 'a' + 1).ToArray();
+            input = Console.ReadLine();
+        }
+
+        public void Solution()
+        {
+            Init();
+            foreach (char c in input)
+                Cnt[c - 'a']++;
+            foreach (int i in Cnt)
+                Console.Write(i+" ");
+        }
+    }
+}
