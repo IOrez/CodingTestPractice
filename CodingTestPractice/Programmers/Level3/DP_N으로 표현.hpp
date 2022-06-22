5701cf4a4922f4114c92b48e42f32a31fe3f77c0
IOrez <h.5363@daum.net>
2022-06-23 00:17:41 +0900

    프로그래머스 Lv3 N으로 표현 새롭게 풀기

--- CodingTestPractice/Programmers/Level3/DP_N으로 표현.hpp
+++ CodingTestPractice/Programmers/Level3/DP_N으로 표현.hpp
@@ -3,31 +3,32 @@
 #include <algorithm>
 using namespace std;
 
-#define MAX 9
-int minDepth = MAX;
-void dfs(int N, int number, int depth, int num) {
-    if (depth >= MAX)
+#define MAX 8
+
+int minDepth = MAX + 1;
+void dfs(int depth, int target, int N, int current)
+{
+    if (depth > MAX || minDepth < depth)
         return;
-    if (num == number)
-        minDepth = min(minDepth, depth);
-
-    int operand = 0;
-    for (int i = 1; i < MAX - depth; ++i) {
-        operand = operand * 10 + N;
-        dfs(N, number, depth + i, num + operand);
-        dfs(N, number, depth + i, num - operand);
-
-        if (num != 0) {
-            dfs(N, number, depth + i, num * operand);
-            dfs(N, number, depth + i, num / operand);
-        }
+    if (target == current)
+    {
+        minDepth = min(depth, minDepth);
+        return;
     }
 
+    int operand = 0;
+    for (int i = 1; i <= MAX - depth; ++i)
+    {
+        operand = operand * 10 + N;
+        dfs(depth + i, target, N, current + operand);
+        dfs(depth + i, target, N, current - operand);
+        dfs(depth + i, target, N, current * operand);
+        dfs(depth + i, target, N, current / operand);
+    }
+    return;
 }
 
 int solution(int N, int number) {
-    int answer = 0;
-    dfs(N, number, 0, 0);
-    answer = (minDepth >= MAX) ? -1 : minDepth;
-    return answer;
+    dfs(0, number, N, 0);
+    return (minDepth > MAX) ? -1 : minDepth;
 }
